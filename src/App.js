878edfcb3a737caf1dbc27105ad1aa5b78d3cd45
Kevin Z <zhangk7532@gmail.com>
2021-09-27 16:46:32 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,175 @@
+//import logo from './logo.svg';
+import './App.css';
+import React, { useEffect, useState } from "react";
+import Weather from './components/weather';
+function App() {
+  const[lat, setLat] = useState([]);
+  const[long, setLong] = useState([]);
+  const[data, setData] = useState([]);
+  const[location,setLocation] = useState([]);
+  const[cityName,setName] = useState([]);
+
+  navigator.geolocation.getCurrentPosition(function(position) {
+    setLat(position.coords.latitude);
+    setLong(position.coords.longitude);
+  });
+
+ 
+  useEffect(() => {
+    
+    //console.log(lat);
+    //console.log(long);
+    getLocation(lat,long)
+    .then(res=>{
+      setLocation(res);
+      try {
+        //var tempob = JSON.parse(d);
+        console.log(res.address.city)
+        //var save = res.address.city;
+        setName(res.address.city);
+      
+      } catch(e) {
+        console.log(e);
+      }
+    });
+    
+    
+    getWeather()
+    .then(res=> {
+      try {
+        setData(res);
+      
+      } catch(e) {
+        console.log(e);
+      }
+      
+    });
+    
+  
+    
+   
+
+    
+    ///data["name"] = location.City;
+
+    /** 
+    const fetchData = async () => {
+      
+      
+      //console.log(process.env.REACT_APP_API_URL);
+
+      
+      //first
+      //await fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&appid=60cae35d1b9a7652148615f5c4e504ac`)
+      //second
+
+      await fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${long}&appid=8fa03f46691074555e14ebe2d2a07cce`)
+      .then(res => res.json())
+      .then(result => {
+        setData(result);
+        //console.log(result);
+      });
+      /*
+      await fetch(`https://us1.locationiq.com/v1/reverse.php?key=pk.25830325a0c832028916fe2ec70cfbda&lat=${lat}&lon=${long}&format=json`)
+      .then(res => res.json())
+      .then(result => {
+        setData(result);
+        console.log(result);
+      });
+      
+
+    }
+    const fetchLocation = async () => {
+      
+      await fetch(`https://us1.locationiq.com/v1/reverse.php?key=pk.25830325a0c832028916fe2ec70cfbda&lat=${lat}&lon=${long}&format=json`)
+      .then(res => res.json())
+      .then(result => {
+        setLocation(result);
+        //console.log(location["address"].city);
+        //console.log(result);
+      });
+      
+
+    }
+    
+    fetchData();
+    fetchLocation();
+    */
+    
+    function getWeather() {
+      return fetch(
+        `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=1cc9d32cdb1cdf53293e1aad91562a89`
+      )
+        .then(res => res.json())
+        .then(res => {
+          /** 
+          if (Object.entries(weather).length) {
+            const mappedData = mapDataToWeatherInterface(weather);
+            return mappedData;
+          }
+          **/
+         //res["name"] = "stupid";
+         return res;
+        });
+    }
+
+  }, [lat,long,cityName])
+  /** 
+  function cityName(d){
+    
+    try {
+      //var tempob = JSON.parse(d);
+      console.log(d.address.city)
+    
+    } catch(e) {
+      console.log(e);
+    }
+  }
+  */
+
+  
+  function getLocation(lat, long) {
+    return fetch(
+      `https://us1.locationiq.com/v1/reverse.php?key=pk.25830325a0c832028916fe2ec70cfbda&lat=${lat}&lon=${long}&format=json`
+    )
+      .then(res => res.json())
+      .then(res => {
+        /** 
+        if (Object.entries(weather).length) {
+          const mappedData = mapDataToWeatherInterface(weather);
+          return mappedData;
+        }
+        **/
+       //data["city"] = res.address.city;
+       return res;
+      });
+  }
+
+  console.log(data);
+  console.log(location);
+  console.log("city name " + cityName);
+  
+
+
+  return (
+    <div className="App">
+      {(typeof data.main != 'undefined') ? (
+       
+          
+          <Weather weatherData={data}/>
+          
+        
+      ): (
+        <div></div>
+        
+      )}
+
+
+
+    </div>
+
+    
+  );
+}
+
+export default App;
---
+++ src/components/weather.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import './styles.css';
+import moment from 'moment';
+
+const WeatherCard = ({weatherData}) => (
+  <div className="main">
+      <p className="header">{weatherData.name}</p>
+      <div className="flex">
+        <p className="day">{moment().format('dddd')}, <span>{moment().format('LL')}</span></p>
+        <p className="description">{weatherData.weather[0].main}</p>
+      </div>
+
+      <div className="flex">
+        <p className="temp">Temprature: {tempConvert(weatherData.main.temp)}&deg;F </p>
+        <p className="temp">Humidity: {weatherData.main.humidity} %</p>
+        
+      </div>
+
+      <div className="flex">
+        <p className="sunrise-sunset">Hi:{tempConvert(weatherData.main.temp_max)}&deg;F Low:{tempConvert(weatherData.main.temp_min)}&deg;F</p>
+        <p className="sunrise-sunset">Wind Speed: {weatherData.wind.speed} MPH</p>
+      </div>
+
+
+
+      <div className="flex">
+        <p className="sunrise-sunset">Sunrise: {new Date(weatherData.sys.sunrise * 1000).toLocaleTimeString('en-IN')}</p>
+        <p className="sunrise-sunset">Sunset: {new Date(weatherData.sys.sunset * 1000).toLocaleTimeString('en-IN')}</p>
+      </div>
+    
+  </div>
+)
+function tempConvert(kelvin){
+  return (((kelvin -273.15)*1.8)+32).toFixed(0);
+}
+
+export default WeatherCard;
